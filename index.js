cd5ec36d7c1c757a9299c54cebf4726e27573186
Manny273 <134006013+Manny273@users.noreply.github.com>
2025-05-01 00:06:11 +0000

    finished

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post("/add/teacher", async (req, res) => {
 
 // Add your SRV string, make sure that the database is called CSHteachers
 async function startServer() {
-  await mongoose.connect("...");
+  await mongoose.connect("mongodb+srv://SE12:CSH2029@cluster0.bwhqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
   app.listen(3000, () => {
     console.log(`Server running.`);
---
+++ public/teacherScript.js
@@ -0,0 +1,22 @@
+// Add an event listener to the form that upon submits creates a new teacher
+// When the teacher is created redirect to the teachers page
+const form = document.querySelector("form")
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    const bookData = new FormData(form)
+    const req = Object.fromEntries(bookData)
+
+    const response = await fetch("/add/teacher", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        }, 
+        body: JSON.stringify(req)
+    })
+    const data = await response.json()
+    console.log(data)
+
+    window.location.href = "/"
+})
